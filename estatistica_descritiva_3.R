f94867be707b3541fc456e8e9bea6dbc68da8fb6
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-18 00:42:48 -0300

    Script

--- estatistica_descritiva_3.R
+++ estatistica_descritiva_3.R
@@ -32,13 +32,17 @@ d <- dados %>%
             se = sd(weight) / sqrt(length(weight)))
 d
 
+d <- arrange(d, desc(media_feed))
+d
+
 # Etapas da produção dos gráficos ----------------------------------------------------------------------------------------------------------
 
 # 1. Definir variáveis e tipo de gráfico;
 # 2. Definir títulos dos eixos;
-# 3. Acrescentar um tema ao gráfico, reduzir largura das barras e retirar a legenda
-# 4. Transformar mês em variável fatorial e mudar nome dos eixos
-# 5. Nomear os gráficos e uni-los em uma janela.
+# 3. Acrescentar um tema ao gráfico;
+# 4. Reduzir largura das barras e retirar a legenda;
+# 5. Mudar nome dos eixos;
+# 6. Ordenar as barras da maior para menor.
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
 
@@ -46,15 +50,20 @@ library(ggplot2) # Pacote para produzir os gráficos
 #install.packages("hrbrthemes")
 library(hrbrthemes) # Pacote para mudar o tema do gráfico (cor preta)
 
+positions <- c("sunflower", "casein", "meatmeal", "soybean", "linseed", "horsebean")
+
 ggplot(d, aes(x = feed, y = media_feed, fill = feed)) +
   geom_col(width = 0.6) +
-  geom_text(aes(x = feed,
-                label = round(media_feed)), 
-                size = 4, vjust = 0.03) +
-  #scale_x_discrete(breaks = c(casein, horsebean, linseed, 
-                              #meatmeal, soybean, sunflower),
-                   #labels = c("Caseína", "Feijão", "Linhaça",
-                              #"Almôndegas", "Soja", "Girassol")) +
+  geom_errorbar(aes(x = feed, ymin = media_feed - se, 
+                ymax = media_feed + se), 
+                width = 0.14, position = position_dodge(.9), size = 0.9) +
+  scale_fill_manual(values = c("#1b9e77", "#d95f02", "#7570b3",
+                               "#e7298a", "#66a61e", "#e6ab02")) +
+  scale_x_discrete(limits = positions,
+    breaks = c("casein", "horsebean", "linseed", 
+                              "meatmeal", "soybean", "sunflower"),
+    labels = c("Caseína", "Feijão", "Linhaça",
+                              "Almôndegas", "Soja", "Girassol")) +
   labs(x = "Suplemento alimentar", y = "Peso (g)") +
   theme_ipsum_rc(base_size = 10) +
   theme(legend.position = "none")
