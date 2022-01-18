8fbcde1c61ccc2fdb1d5a1db0eff67822a369c39
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-17 23:53:30 -0300

    Script

--- estatistica_descritiva_3.R
+++ estatistica_descritiva_3.R
@@ -12,6 +12,14 @@ dados
 ### Experimento conduzido para medir e comparar a efetividade de vários suplementos
 ### alimentares sobre a taxa de crescimento de galinhas.
 
+### Lista dos suplementos alimentares:
+# 1. Caseína
+# 2. Feijão
+# 3. Linhaça
+# 4. Almôndegas
+# 5. Soja
+# 6. Girassol
+
 # Estatística descritiva dos dados ---------------------------------------------------------------------------------------------------------
 
 library(dplyr) # Pacote usado para análises descritivas
@@ -38,10 +46,10 @@ library(ggplot2) # Pacote para produzir os gráficos
 #install.packages("hrbrthemes")
 library(hrbrthemes) # Pacote para mudar o tema do gráfico (cor preta)
 
-ggplot(d, aes(x = Month, y = media_temp, fill = Month)) +
+ggplot(d, aes(x = feed, y = media_feed, fill = feed)) +
   geom_col(width = 0.6) +
-  geom_text(aes(x = Month,
-                label = round(media_temp)), 
+  geom_text(aes(x = feed,
+                label = round(media_feed)), 
                 size = 4, vjust = 0.03) +
   scale_x_discrete(breaks = c(5, 6, 7, 8, 9),
                    labels = c("Maio", "Junho", "Julho",
